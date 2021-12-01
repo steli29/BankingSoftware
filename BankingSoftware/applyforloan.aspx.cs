ab8779a8c8840f627af9fbb16fea9482b4f329ed
ChiChiMiti <dtr.todorov@abv.bg>
2021-12-01 18:52:52 +0200

    #16

--- BankingSoftware/applyforloan.aspx.cs
+++ BankingSoftware/applyforloan.aspx.cs
@@ -13,5 +13,43 @@ namespace BankingSoftware
         {
 
         }
+
+        protected void SubmitLoan_Click(object sender, EventArgs e)
+        {
+
+            string LoanType = DropDownList1.SelectedValue;
+            string Name= FullName.Text.ToString();
+            double MonthlyIncome= double.Parse(NMI.Text.ToString());
+            double AmountNeeded = double.Parse(MoneyLoan.Text.ToString());
+
+            double SixMonthsPayment = 6 * MonthlyIncome;
+            double percent=0;
+            if (LoanType == "HomeLoan") 
+            {
+                percent = 0.1;
+            }
+            else if(LoanType =="CarLoan")
+            {
+                percent = 0.2;
+            }
+            else if(LoanType=="PersonalLoan")
+            {
+                percent = 0.3;
+            }
+            else if(LoanType=="EducationalLoan")
+            {
+                percent = 0.09;
+            }
+            double MoneyToPayForSixMonths = percent * AmountNeeded;
+            if (MoneyToPayForSixMonths > SixMonthsPayment)
+            {
+                Response.Write("<script>alert('No');</script>");
+            }
+            else 
+            {
+                Response.Write("<script>alert('Yes');</script>");
+            }
+
+        }
     }
 }
