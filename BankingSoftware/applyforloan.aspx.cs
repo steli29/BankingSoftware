d2990572560334fdbc11a8448ac61a2d048af78c
ChiChiMiti <dtr.todorov@abv.bg>
2021-12-06 17:55:06 +0200

    #21

--- BankingSoftware/applyforloan.aspx.cs
+++ BankingSoftware/applyforloan.aspx.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace BankingSoftware
 {
     public partial class applyforloan : System.Web.UI.Page
     {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -18,35 +18,66 @@ namespace BankingSoftware
         {
             string LoanType = DropDownList1.SelectedValue;
             string Name = FullName.Text.ToString();
-            double MonthlyIncome = double.Parse(NMI.Text.ToString());
-            double AmountNeeded = double.Parse(MoneyLoan.Text.ToString());
+            decimal MonthlyIncome = decimal.Parse(NMI.Text.ToString());
+            decimal AmountNeeded = decimal.Parse(MoneyLoan.Text.ToString());
 
-            double SixMonthsPayment = 6 * MonthlyIncome;
-            double percent = 0;
+            decimal SixMonthsPayment = 6 * MonthlyIncome;
+            decimal percent = 0;
             if (LoanType == "HomeLoan")
             {
-                percent = 0.1;
+                percent = 0.1m;
             }
             else if (LoanType == "CarLoan")
             {
-                percent = 0.2;
+                percent = 0.2m;
             }
             else if (LoanType == "PersonalLoan")
             {
-                percent = 0.3;
+                percent = 0.3m;
             }
             else if (LoanType == "EducationalLoan")
             {
-                percent = 0.09;
+                percent = 0.09m;
             }
-            double MoneyToPayForSixMonths = percent * AmountNeeded;
+            decimal MoneyToPayForSixMonths = percent * AmountNeeded;
             if (MoneyToPayForSixMonths > SixMonthsPayment)
             {
                 Response.Write("<script>alert('No');</script>");
             }
             else
             {
-                Response.Write("<script>alert('Yes');</script>");
+                try
+                {
+                    SqlConnection con = new SqlConnection(strcon);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    string _Cash = MoneyLoan.Text.Replace('.', ',').Trim();
+                    decimal newBalance = decimal.Parse(Session["balance"].ToString()) + decimal.Parse(_Cash);
+                    DateTime date = DateTime.Today;
+                    SqlCommand cmd = new SqlCommand("INSERT INTO balance_tbl (user_id, new_balance, date, transaction_amount, info)" +
+                        " values(@user_id, @new_balance, @date, @transaction_amount, @info)", con);
+                    cmd.Parameters.AddWithValue("@user_id", Session["user_id"].ToString());
+                    cmd.Parameters.AddWithValue("@new_balance", newBalance);
+                    cmd.Parameters.AddWithValue("@date", date);
+                    cmd.Parameters.AddWithValue("@transaction_amount", decimal.Parse(_Cash));
+                    cmd.Parameters.AddWithValue("@info", "Cerdit transaction amount - "+AmountNeeded.ToString());
+                    cmd.ExecuteNonQuery();
+                    Session["balance"] = newBalance;
+
+                    _Cash = newBalance.ToString();
+                    cmd = new SqlCommand("UPDATE users_tbl SET balance = '" + _Cash.Replace(',', '.').Trim()
+                        + "'WHERE user_id = '" + Session["user_id"].ToString() + "'", con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                catch(Exception ex)
+                {
+
+                }
+                Response.Redirect("viewBalance.aspx");
+
             }
 
         }
